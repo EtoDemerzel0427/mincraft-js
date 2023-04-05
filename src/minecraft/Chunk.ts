5e73e78230ab91521c303370db47241093b36497
EtoDemerzel0427 <huangweiran1998@gmail.com>
2023-04-04 19:48:35 -0500

    fixed value noise

--- src/minecraft/Chunk.ts
+++ src/minecraft/Chunk.ts
@@ -1,6 +1,9 @@
-import { Mat3, Mat4, Vec3, Vec4 } from "../lib/TSM.js";
 import Rand from "../lib/rand-seed/Rand.js"
 import ValueNoise from "./noise.js";
+
+function lerp(lo: number, hi: number, t: number): number {
+    return lo * (1 - t) + hi * t;
+}
 
 
 export class Chunk {
@@ -18,35 +21,35 @@ export class Chunk {
         this.generateCubes();
     }
 
-    private generateHeightMap(seed: string): Float32Array {
-        let noises = new ValueNoise(seed, 4);
-        let heightMap = new Float32Array(this.size * this.size).fill(0.);
-        let frequency = 0.02;
-        let frequencyMultiplier = 2;
-        let amplitudeMultiplier = 0.5;
-        const octaves = 5;
-        let maxNoise = 0.0;
-        for (let j = 0; j < this.size; j++) {
-            for (let i = 0; i < this.size; i++) {
-                let x = i * frequency;
-                let y = j * frequency;
-                let amplitude = 1.0;
-                for (let k = 0; k < octaves; k++) {
-                    heightMap[this.size * j + i] += amplitude * noises.eval(x, y);
-                    x *= frequencyMultiplier;
-                    y *= frequencyMultiplier;
-                    amplitude *= amplitudeMultiplier;
-                }
-                maxNoise = Math.max(maxNoise, heightMap[this.size * j + i]);
-            }
-        }
-
-        for (let j = 0; j < this.size * this.size; j++) {
-            heightMap[j]  *= (100 / maxNoise);
-            // console.log(heightMap[j]);
-        }
-        return heightMap;
-    }
+    // private generateHeightMap(seed: string): Float32Array {
+    //     let noises = new ValueNoise(seed, 8, 64);
+    //     let heightMap = new Float32Array(this.size * this.size).fill(0.);
+    //     let frequency = 0.02;
+    //     let frequencyMultiplier = 2;
+    //     let amplitudeMultiplier = 0.5;
+    //     const octaves = 3;
+    //     let maxNoise = 0.0;
+    //     for (let j = 0; j < this.size; j++) {
+    //         for (let i = 0; i < this.size; i++) {
+    //             let x = i * frequency;
+    //             let y = j * frequency;
+    //             let amplitude = 1.0;
+    //             for (let k = 0; k < octaves; k++) {
+    //                 heightMap[this.size * j + i] += amplitude * noises.eval(x, y);
+    //                 x *= frequencyMultiplier;
+    //                 y *= frequencyMultiplier;
+    //                 amplitude *= amplitudeMultiplier;
+    //             }
+    //             maxNoise = Math.max(maxNoise, heightMap[this.size * j + i]);
+    //         }
+    //     }
+    //
+    //     for (let j = 0; j < this.size * this.size; j++) {
+    //         heightMap[j]  *= (100 / maxNoise);
+    //         // console.log(heightMap[j]);
+    //     }
+    //     return heightMap;
+    // }
     
     
     private generateCubes() {
@@ -57,23 +60,25 @@ export class Chunk {
       this.cubes = this.size * this.size;
       this.cubePositionsF32 = new Float32Array(4 * this.cubes);
 
-      const seed = this.x + " " + this.y; // set different seeds for different chunks
-      const heightMap = this.generateHeightMap(seed);
 
-      for(let i=0; i<this.size; i++)
-      {
-          for(let j=0; j<this.size; j++)
-          {
-            // const height = Math.floor(10.0 * rng.next());
-            const height = heightMap[this.size * i + j];
-            const idx = this.size * i + j;
-            this.cubePositionsF32[4*idx + 0] = topleftx + j;
+      const seedInt = this.x + this.y;
+      const seed = seedInt.toString();
+
+      let noises = new ValueNoise(seed, 8, 64);
+      noises.generateBaseMap();
+
+      const heightMap = noises.getHeights();
+
+      for(let i= 0; i < this.size; i++) {
+          for(let j= 0; j<this.size; j++) {
+              const height = heightMap[i * this.size + j];
+              const idx = this.size * i + j;
+            this.cubePositionsF32[4*idx] = topleftx + j;
             this.cubePositionsF32[4*idx + 1] = height;
             this.cubePositionsF32[4*idx + 2] = toplefty + i;
             this.cubePositionsF32[4*idx + 3] = 0;
           }
       }
-    
     }
     
     public cubePositions(): Float32Array {
