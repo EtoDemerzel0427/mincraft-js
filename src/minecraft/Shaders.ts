b751f740c2ab7e8072b5fdfd81062d02fbbf5899
sicongc <sicongche@gmail.com>
2023-04-06 16:26:38 -0500

    implement the Perlin Noise function, but the texture is still buggy

--- src/minecraft/Shaders.ts
+++ src/minecraft/Shaders.ts
@@ -13,6 +13,7 @@ export const blankCubeVSText = `
     varying vec4 normal;
     varying vec4 wsPos;
     varying vec2 uv;
+    varying float seed;
 
     void main () {
 
@@ -20,6 +21,10 @@ export const blankCubeVSText = `
         wsPos = aVertPos + aOffset;
         normal = normalize(aNorm);
         uv = aUV;
+
+        // set seed, every vertex in the same cube has the same aOffset,
+        // so vertices within the same cube has the same seed.
+        seed = aOffset.x*2.0 + aOffset.y*4.0 + aOffset.z*8.0;
     }
 `;
 
@@ -31,7 +36,52 @@ export const blankCubeFSText = `
     varying vec4 normal;
     varying vec4 wsPos;
     varying vec2 uv;
-    
+    varying float seed;
+
+    float random (in vec2 pt, in float seed) {
+        return fract(sin( (seed + dot(pt.xy, vec2(12.9898,78.233))))*43758.5453123);
+    }
+
+    vec2 unit_vec(in vec2 xy, in float seed) {
+        float theta = 6.28318530718*random(xy, seed);
+        return vec2(cos(theta), sin(theta));
+    }
+
+    float smoothmix (float lo, float hi, float t) {
+        return (hi - lo) * (3.0 - t * 2.0) * t * t + lo;
+    }
+
+    float getDot(float cx, float cy, float x, float y, float seed){
+        //generate random vector for corner point
+        vec2 random_vec = unit_vec(vec2(cx,cy),seed);
+
+        //vector, from corner to (x,y)
+        vec2 d = vec2(x-cx,y-cy); 
+
+        //return the dot
+        return dot(d,random_vec);
+    }
+
+    float perlin(float u, float v,float seed) {
+        float x0 = 0.0;
+        float y0 = 0.0;
+        float x1 = 1.0;
+        float y1 = 1.0;
+
+        float h0, h1, i0, i1, value;
+         
+        h0 = getDot(x0, y0, u, v,seed);
+        h1 = getDot(x1, y0, u, v,seed);
+        i0 = smoothmix(h0, h1, u);
+         
+        h0 = getDot(x0, y1, u, v,seed);
+        h1 = getDot(x1, y1, u, v,seed);
+        i1 = smoothmix(h0, h1, u);
+         
+        value = smoothmix(i0, i1, v);
+        return value*0.5+0.5; 
+    }
+
     void main() {
         vec3 kd = vec3(1.0, 1.0, 1.0);
         vec3 ka = vec3(0.1, 0.1, 0.1);
@@ -40,8 +90,15 @@ export const blankCubeFSText = `
         vec4 lightDirection = uLightPos - wsPos;
         float dot_nl = dot(normalize(lightDirection), normalize(normal));
 	    dot_nl = clamp(dot_nl, 0.0, 1.0);
-	
-        gl_FragColor = vec4(clamp(ka + dot_nl * kd, 0.0, 1.0), 1.0);
+        
+        if(normal.x==0.0 && normal.z==0.0 && normal.y>0.0){
+            float p = perlin(uv.x,uv.y,seed);
+            //float t = abs(sin((uv.x+uv.y)*2.0*3.1415));
+            gl_FragColor = vec4(p,p,p,1.0);
+        }
+        else{
+            gl_FragColor = vec4(clamp(ka + dot_nl * kd, 0.0, 1.0), 1.0);
+        }
     }
 `;
 
